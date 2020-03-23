b74420a4ff920249b6a8c6156b561e54b2452354
uebn - bloco de notas...// <62443917+uebn@users.noreply.github.com>
2020-03-23 17:55:56 +0000

    Add files via upload

---
+++ takeaway/qgis2web_2020_03_23-17_16_25_734924/data/restaurantesERV_2.js
@@ -0,0 +1,32 @@
+var json_restaurantesERV_2 = {
+"type": "FeatureCollection",
+"name": "restaurantesERV_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Restaurant": "Cantinho Da Ti Bilete", "Telefone": "266 702 397", "Telemóvel": null, "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.895455, 38.574825 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "Bella Pizza Évora", "Telefone": "266 046 721", "Telemóvel": null, "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.912402, 38.568109 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "Etc & Tal - Pronto a Comer", "Telefone": "266 010 038", "Telemóvel": null, "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.91199, 38.573598 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "Rei dos Frangos", "Telefone": null, "Telemóvel": "968 482 147", "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.900721, 38.583608 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "Origens Restaurante", "Telefone": null, "Telemóvel": "964 220 790", "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.90873, 38.571442 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "A Cozinha da Carmo", "Telefone": "266 701 051", "Telemóvel": null, "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.898378, 38.564573 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "Restaurante Abadia", "Telefone": null, "Telemóvel": "966 066 972", "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.910719, 38.573204 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "Restaurante São Luís", "Telefone": "266 741 585", "Telemóvel": null, "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.910885, 38.568968 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "O Moinho do Cu Torto", "Telefone": "266 771 060", "Telemóvel": null, "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.916781, 38.555146 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "O Pitéu", "Telefone": "266 703 410", "Telemóvel": null, "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.90628, 38.564959 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "Sabores da Mostardeira", "Telefone": "266 707 772", "Telemóvel": null, "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.912266, 38.574528 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "Adega da Talha Velha", "Telefone": null, "Telemóvel": "968 506 112", "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.910259, 38.56896 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "O Combinado", "Telefone": "266 700 627", "Telemóvel": null, "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.902373, 38.572673 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "Sabores do Alentejo", "Telefone": "266 103 956", "Telemóvel": null, "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.908494, 38.59082 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "Gaveta d' Ingredientes", "Telefone": null, "Telemóvel": "963 541 239", "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.895367, 38.575439 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "Tenda D'El Rey", "Telefone": "266 701 499", "Telemóvel": null, "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.900401, 38.56501 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "Churrasqueira São Tiago", "Telefone": "266 707 284", "Telemóvel": null, "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.902614, 38.565966 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "Restaurante Barraca de Pau", "Telefone": "266 733 655", "Telemóvel": null, "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.955492, 38.567436 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "Take-away do Mercado", "Telefone": "266 743 366", "Telemóvel": null, "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.902904, 38.590418 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "Restaurante Michel", "Telefone": "266 704 880", "Telemóvel": "926 932 031", "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.902457, 38.584302 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "A Fragata", "Telefone": "266 044 255", "Telemóvel": null, "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.925422, 38.569589 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "Chocolate d'El Rei", "Telefone": "266 706 979", "Telemóvel": null, "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.901956, 38.565606 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "Café Alentejo", "Telefone": "266 706 296", "Telemóvel": null, "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.909596, 38.570503 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "A Choupana", "Telefone": "266 704 427", "Telemóvel": null, "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.90984, 38.570642 ] } },
+{ "type": "Feature", "properties": { "Restaurant": "Restaurante Casa Cravosa", "Telefone": null, "Telemóvel": "962 539 718", "Email": null }, "geometry": { "type": "Point", "coordinates": [ -7.900807, 38.574022 ] } }
+]
+}
